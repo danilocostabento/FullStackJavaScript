fca1458bda99f56c09c665072d549665f5c292d9
Dann074 <91625647+Dann074@users.noreply.github.com>
2024-03-26 16:00:59 -0300

    Update and rename Activity 7.2 - Pile of Playing Cartds to Activity 7.2 - Pile of Playing Cartds.js

--- Activity 7.2 - Pile of Playing Cartds.js
+++ Activity 7.2 - Pile of Playing Cartds.js
@@ -1,10 +1,10 @@
-Write a JavaScript program to simulate a deck of cards. The program should start
+/*Write a JavaScript program to simulate a deck of cards. The program should start
 by showing an interactive menu on the screen containing the number of cards currently
 in the deck and the options to “Add a card”, “Draw a card” and “Exit”. When choosing 
 “Add a card”, the program should ask for the name of the card and add it to the top 
 of the deck. When choosing “Pull a card”, the program must remove the card from the 
 top of the deck and show the name of the card drawn on the screen. The program must 
-only be closed by choosing the “Exit” option, otherwise you must return to the menu.
+only be closed by choosing the “Exit” option, otherwise you must return to the menu.*/
 
 
 let cards = [`Ace of Hearts`, `Ace of Clubs`, `Ace of Diamonds`, `Ace of Spades`, `Two of Hearts`, `Two of Clubs`, `Two of Diamonds`, `Two of Spades`, `Three of Hearts`, `Three of Clubs`, `Three of Diamonds`, `Three of Spades`, `Four of Hearts`, `Four of Clubs`, `Four of Diamonds`, `Four of Spades`, `Five of Hearts`, `Five of Clubs`, `Five of Diamonds`, `Five of Spades`, `Six of Hearts`, `Six of Clubs`, `Six of Diamonds`, `Six of Spades`, `Seven of Hearts`, `Seven of Clubs`, `Seven of Diamonds`, `Seven of Spades`, `Eight of Hearts`, `Eight of Clubs`, `Eight of Diamonds`, `Eight of Spades`, `Nine of Hearts`, `Nine of Clubs`, `Nine of Diamonds`, `Nine of Spades`, `Ten of Hearts`, `Ten of Clubs`, `Ten of Diamonds`, `Ten of Spades`, `Jack of Hearts`, `Jack of Clubs`, `Jack of Diamonds`, `Jack of Spades`, `Queen of Hearts`, `Queen of Clubs`, `Queen of Diamonds`, `Queen of Spades`, `King of Hearts`, `King of Clubs`, `King of Diamonds`, `King of Spades`, `Red Joker`, `Black Joker`]
