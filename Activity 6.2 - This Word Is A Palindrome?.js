af751fbb6b29bbcfbefd89807c4846ced9ff92c5
Dann074 <91625647+Dann074@users.noreply.github.com>
2024-03-26 16:00:02 -0300

    Update and rename Activity 6.2 - This Word Is A Palindrome? to Activity 6.2 - This Word Is A Palindrome?.js

--- Activity 6.2 - This Word Is A Palindrome?.js
+++ Activity 6.2 - This Word Is A Palindrome?.js
@@ -1,9 +1,9 @@
-Write a JavaScript program that is capable of identifying whether a 
+/*Write a JavaScript program that is capable of identifying whether a 
 word is a palindrome. A palindrome is a word that read backwards has 
 the same letters in the same order. The program must start by asking 
 for a word to be entered and then it must display a message saying 
 whether or not that word is a palindrome. If it is not a palindrome, 
-the program must show the word read from left to right and right to left.
+the program must show the word read from left to right and right to left.*/
 
 
 let word = prompt(`Whats is the word?`)
