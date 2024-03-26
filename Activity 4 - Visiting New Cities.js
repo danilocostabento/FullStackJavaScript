a1f68dba4b201b44600aeeffaf7921948381acd2
Dann074 <91625647+Dann074@users.noreply.github.com>
2024-03-26 15:54:04 -0300

    Update and rename Activity 4 - Visiting New Cities to Activity 4 - Visiting New Cities.js

--- Activity 4 - Visiting New Cities.js
+++ Activity 4 - Visiting New Cities.js
@@ -1,10 +1,10 @@
-Write a JavaScript program that asks for a tourist's
+/*Write a JavaScript program that asks for a tourist's
 name and then asks if they have visited a city. 
 If the answer is yes, ask for the name of the city and 
 store it in a variable, and then continue asking if the 
 tourist visited any other city until the answer is no. 
 At the end, the program must display the tourist's name, 
-how many and which cities he visited.
+how many and which cities he visited.*/
 
 let name = prompt("What´s your name?")
 var fly = prompt(`Have you visited any city?
