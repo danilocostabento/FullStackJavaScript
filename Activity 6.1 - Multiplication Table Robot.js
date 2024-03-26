1b569d7e2f09ae012205ce6c903efbd50f59798a
Dann074 <91625647+Dann074@users.noreply.github.com>
2024-03-26 15:59:41 -0300

    Update and rename Activity 6.1 - Multiplication Table Robot to Activity 6.1 - Multiplication Table Robot.js

--- Activity 6.1 - Multiplication Table Robot.js
+++ Activity 6.1 - Multiplication Table Robot.js
@@ -1,7 +1,7 @@
-Write a JavaScript program that is capable of calculating the multiplication table of a 
+/*Write a JavaScript program that is capable of calculating the multiplication table of a 
 given number (from 1 to 20). The program must start by asking for the number to be used 
 in the calculations and then save all multiplication results from 1 to 20 using the number 
-entered within a string variable. Then the program should display these results and finish.
+entered within a string variable. Then the program should display these results and finish.*/
 
 var number = parseFloat(prompt(`What's the number you wnat the multiplication table?`))
 let multiplicationTable = ""
